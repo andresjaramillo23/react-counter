8b1441877a3b39c087fd041f21b2f3f5ac0c1ecc
Andres Jaramillo <a.jaramillo24@gmail.com>
2021-10-19 16:50:35 +0000

    Added counter component and clock icon

---
+++ src/js/component/counter.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const Counter = () => {
+	return <div className="d-flex align-items-center far fa-clock fa-7x"></div>;
+};
+
+export default Counter;
--- src/js/index.js
+++ src/js/index.js
@@ -10,6 +10,7 @@ import "../styles/index.scss";
 
 //import your own components
 import Home from "./component/home.jsx";
+import Counter from "./component/counter.jsx";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<Counter />, document.querySelector("#app"));
