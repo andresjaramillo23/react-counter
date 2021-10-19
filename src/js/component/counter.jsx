b83fb6b6c87e0a490f899224d9ae90c24483f4a7
Andres Jaramillo <a.jaramillo24@gmail.com>
2021-10-19 18:25:13 +0000

    Added style for moving the clock to the center of the page

--- src/js/component/counter.jsx
+++ src/js/component/counter.jsx
@@ -2,7 +2,15 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const Counter = () => {
-	return <div className="d-flex align-items-center far fa-clock fa-7x"></div>;
+	return (
+		<div className="container">
+			<div className="row justify-content-md-center">
+				<div className="col-6">
+					<div className="far fa-clock fa-7x"></div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Counter;
