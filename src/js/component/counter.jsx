f2d93cafd1c05d18e589462aaf38d2736490af20
Andres Jaramillo <a.jaramillo24@gmail.com>
2021-11-06 02:10:31 +0000

    Changes

--- src/js/component/counter.jsx
+++ src/js/component/counter.jsx
@@ -1,23 +1,105 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React, { useState } from "react";
 
 const Counter = props => {
+	const [isRunning, setIsRunning] = useState(false);
+	const [buttonState, setButtonState] = useState("Start Timer");
+	const [tiempo, setTiempo] = useState("");
+	const [timeleft, setTimeleft] = useState(10);
+
+	const clickHandler = e => {
+		runtimer(e);
+	};
+
+	function runtimer(e) {
+		if (!e.checked) {
+			setIsRunning(true);
+			setButtonState("Stop Timer");
+		} else {
+			setIsRunning(false);
+			setButtonState("Start Timer");
+			setTimeleft(-1);
+		}
+	}
+
+	if (isRunning) {
+		let timer = new Timer(function() {
+			if (timeleft == 0) {
+				timer.start();
+			}
+			if (timeleft > 0) {
+				setTiempo(timeleft + " seconds remaining");
+				let papo = timeleft - 1;
+				setTimeleft(papo);
+			}
+			if (timeleft == -1) {
+				timer.stop();
+			}
+		}, 1000);
+
+		// else {
+		// 	timeleft = -1;
+		// }
+	}
 	return (
-		<div className="container">
-			<div className="row justify-content-md-center">
-				<div className="col-6">
-					<div className="far fa-clock fa-7x"></div>
-				</div>
-				<div className="col-1">
-					<span>{props.time}</span>
+		<>
+			<div className="container">
+				<div className="row justify-content-md-center">
+					<div className="col-6">
+						<div className="far fa-clock fa-7x"></div>
+					</div>
+					<div className="col-1">
+						<span>{tiempo}</span>
+					</div>
 				</div>
 			</div>
-		</div>
+
+			<div className="container">
+				<div className="row">
+					<div className="col">
+						<input
+							type="checkbox"
+							className="btn-check"
+							id="b2"
+							checked
+							autoComplete="off"
+							onChange={clickHandler}
+						/>
+						<label
+							id="label"
+							className="btn btn-primary"
+							htmlFor="b2">
+							{buttonState}
+						</label>
+					</div>
+				</div>
+			</div>
+		</>
 	);
 };
 
-Counter.propTypes = {
-	time: PropTypes.string
-};
+function Timer(fn, t) {
+	var timerObj = setInterval(fn, t);
+
+	this.stop = function() {
+		if (timerObj) {
+			clearInterval(timerObj);
+			timerObj = null;
+		}
+		return this;
+	};
+
+	this.start = function() {
+		if (!timerObj) {
+			this.stop();
+			timerObj = setInterval(fn, t);
+		}
+		return this;
+	};
+
+	this.reset = function(newT = t) {
+		t = newT;
+		return this.stop().start();
+	};
+}
 
 export default Counter;
--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
-//import react into the bundle
-import React from "react";
+//import React from "react";
 import ReactDOM from "react-dom";
+import React, { useState, useEffect } from "react";
 
 //include bootstrap npm library into the bundle
 import "bootstrap";
@@ -13,61 +13,9 @@ import Home from "./component/home.jsx";
 import Counter from "./component/counter.jsx";
 import Button from "./component/button.jsx";
 
-let timeleft = 10;
-
-const clickHandler = e => {
-	runtimer(e);
-};
-
-function Timer(fn, t) {
-	var timerObj = setInterval(fn, t);
-
-	this.stop = function() {
-		if (timerObj) {
-			clearInterval(timerObj);
-			timerObj = null;
-		}
-		return this;
-	};
-
-	this.start = function() {
-		if (!timerObj) {
-			this.stop();
-			timerObj = setInterval(fn, t);
-		}
-		return this;
-	};
-
-	this.reset = function(newT = t) {
-		t = newT;
-		return this.stop().start();
-	};
-}
-
-function runtimer(e) {
-	if (!e.checked) {
-		let timer = new Timer(function() {
-			if (timeleft == 0) {
-				timeleft = 10;
-			}
-			if (timeleft > 0) {
-				ReactDOM.render(
-					<Counter time={timeleft + " seconds remaining"} />,
-					document.querySelector("#app")
-				);
-				timeleft -= 1;
-			}
-			if (timeleft == -1) {
-				timer.stop();
-			}
-		}, 1000);
-	}
-	// else {
-	// 	timeleft = -1;
-	// }
-}
-
 ReactDOM.render(
-	<Button name="Time Start" function={clickHandler} />,
+	<>
+		<Counter />
+	</>,
 	document.querySelector("#app")
 );
